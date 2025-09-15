75ad5e3b6cc2347dd5bc81acd6c4a49d29d45cb7
Joe Mancuso <idmann509@gmail.com>
2025-09-15 16:13:09 -0400

    refactored

---
+++ users/routes.py
@@ -0,0 +1,8 @@
+from masonite.routes import Route
+from .controllers.UsersController import UsersController
+
+ROUTES = [
+    Route.get("/dashboard/users/all", UsersController.index).name("Manage Users"),
+    Route.get("/dashboard/users/@id/edit", UsersController.edit),
+    Route.post("/dashboard/users/@id/update", UsersController.update)
+]
